a9da057b6ec1560fc83db0733487513f1c8bc364
Raddiya <raddiyasultana.612@gmil.com>
2022-03-26 02:01:27 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 class="text-center p-3 mb-2 bg-info">Make-up Yourself</h1>
+        </div>
+    );
+};
+
+export default Header;
